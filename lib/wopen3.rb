d8a681edcb91b8fb22ab448afce4dfc574b5fd1b
Wincent Colaiuta <win@wincent.com>
2010-07-28 17:59:05 +0200

    Add 'system' method as convenience wrapper for common pattern
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- lib/wopen3.rb
+++ lib/wopen3.rb
@@ -1,4 +1,4 @@
-# Copyright 2007-2008 Wincent Colaiuta
+# Copyright 2007-2010 Wincent Colaiuta
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -14,8 +14,9 @@
 
 # Wopen3 is a replacement for Open3.
 #
-# Unlike Open3, Wopen3 does not throw away the exit code of the executed (grandchild) process.
-# Only a child process is spawned and the exit status is returned in $? as normal.
+# Unlike Open3, Wopen3 does not throw away the exit code of the executed
+# (grandchild) process. Only a child process is spawned and the exit status is
+# returned in $? as normal.
 #
 # Usage example:
 #
@@ -28,53 +29,56 @@
 #     threads << Thread.new(stderr) do |err|
 #       err.each { |line| errors << line }
 #     end
-#     threads.each { |thread| thread.join }      
+#     threads.each { |thread| thread.join }
 #   end
 #   status = $?.exitstatus
 #   raise "Non-zero exit status #{status}" if status != 0
 #
+# As this is such a common usage pattern, a 'system' method is provided as a
+# convenience:
+#
+#   result = Wopen3.system('git', 'log')
+#   result.status # => 0
+#   result.stderr # => ''
+#   result.stdout # => 'commit 491411b3...'
 #
 module Wopen3
-  
   class Opener
-    
     READ  = 0
     WRITE = 1
-    
-    def initialize(*cmd)
-      
+
+    def initialize *cmd
       write_pipe  = IO::pipe
       read_pipe   = IO::pipe
       error_pipe  = IO::pipe
-      
+
       pid = fork do
-        
         # in child
         write_pipe[WRITE].close
         STDIN.reopen(write_pipe[READ])    # file descriptor duplicated here
-        write_pipe[READ].close            # must close in order for parent to see EOF
-        
+        write_pipe[READ].close            # must close in order for parent to
+                                          # see EOF
         read_pipe[READ].close
         STDOUT.reopen(read_pipe[WRITE])
         read_pipe[WRITE].close
-        
+
         error_pipe[READ].close
         STDERR.reopen(error_pipe[WRITE])
         error_pipe[WRITE].close
-        
-        exec(*cmd)  # will raise SystemCallError if command can't be executed (eg Errno::NOENT)
+
+        exec(*cmd)  # will raise SystemCallError if command can't be executed
+                    # (eg Errno::NOENT)
         exit!       # should never get here
-        
       end
-      
+
       # in parent
       write_pipe[READ].close
       read_pipe[WRITE].close
       error_pipe[WRITE].close
-      
+
       pipes = [write_pipe[WRITE], read_pipe[READ], error_pipe[READ]]
       write_pipe[WRITE].sync = true
-      
+
       if defined? yield
         begin
           result = yield(*pipes)
@@ -84,17 +88,40 @@ module Wopen3
           pipes.each { |pipe| pipe.close unless pipe.closed? }
         end
       end
-      
-      # can't wait immediately (will hang); wait once garbage collection destroys/finalizes this instance
+
+      # can't wait immediately (will hang); wait once garbage collection
+      # destroys/finalizes this instance
       ObjectSpace.define_finalizer(self, proc { Process.waitpid(pid) })
       pipes
-      
     end
-    
+  end # class Opener
+
+  class Result
+    attr_reader :args, :status, :stderr, :stdout
+
+    def initialize *args
+      @args = args
+      @stderr = ''
+      @stdout = ''
+      Wopen3.popen3 *args do |stdin, stdout, stderr|
+        threads = []
+        threads << Thread.new(stdout) do |out|
+          out.each { |line| @stdout << line }
+        end
+        threads << Thread.new(stderr) do |err|
+          err.each { |line| @stderr << line }
+        end
+        threads.each { |thread| thread.join }
+      end
+      @status = $?.exitstatus
+    end
+  end # class Result
+
+  def self.popen3 *cmd, &block
+    Opener.new *cmd, &block
   end
-  
-  def self.popen3(*cmd, &block)
-    Opener.new(*cmd, &block)
+
+  def self.system *args
+    Result.new *args
   end
-  
 end # module Wopen3
